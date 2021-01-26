79c92d8c3996ff55471dd5d8018d86dad5507ac9
semcak1 <semihcakmak1@hotmail.com>
2021-01-26 17:37:33 +0300

    1 file modified

--- index.js
+++ index.js
@@ -5,31 +5,14 @@ import "./style.css";
 const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-const FourEach = (arr, fn) => {
-  for (let i = 0; i < arr.length; i++) {
-    fn(arr[i]);
-  }
+const outer = (f) => {
+  const inner= () => {
+    console.log(f);
+  };
+  return inner
 };
 
-let arr1 = [10, 25, 36, 4, 54, 64, 7, 81];
 
-//verilen dizideki elemanın çift mi ?
-const isEven = number => {
-  if (!(number % 2)) {
-    return console.log(number, "is even");
-  }
-};
 
-//verilen dizideki elemanın karekoku var mı ?
-const hasSquareRoot = number => {
-  const sqr = Math.sqrt(number);
-  if ((sqr * 10) % 10 === 0) {
-    let sqrNum = Math.sqrt(number);
-    return console.log(number, "is squre of", sqrNum);
-  }
-};
-
-FourEach(arr1, num => {
-  // isEven(num)
-  hasSquareRoot(num);
-});
+const anyFunc = outer(5);
+anyFunc();
