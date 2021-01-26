e6c541affa29f5b4acbbf7f03daa581327ba7cfd
semcak1 <semihcakmak1@hotmail.com>
2021-01-26 17:46:41 +0300

    1 file modified

--- index.js
+++ index.js
@@ -5,31 +5,13 @@ import "./style.css";
 const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-const FourEach = (arr, fn) => {
-  for (let i = 0; i < arr.length; i++) {
-    fn(arr[i]);
-  }
+const outer = value => {
+  return fn => {
+    typeof fn === "function" && fn(value);
+    console.log(value);
+  };
 };
 
-let arr1 = [10, 25, 36, 4, 54, 64, 7, 81];
-
-//verilen dizideki elemanın çift mi ?
-const isEven = number => {
-  if (!(number % 2)) {
-    return console.log(number, "is even");
-  }
-};
-
-//verilen dizideki elemanın karekoku var mı ?
-const hasSquareRoot = number => {
-  const sqr = Math.sqrt(number);
-  if ((sqr * 10) % 10 === 0) {
-    let sqrNum = Math.sqrt(number);
-    return console.log(number, "is squre of", sqrNum);
-  }
-};
-
-FourEach(arr1, num => {
-  // isEven(num)
-  hasSquareRoot(num);
+outer(5)(val => {
+  console.log("Value is ", val);
 });
