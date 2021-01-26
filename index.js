0e044d650c9684b7de210e42882fbab2f8b527d6
semcak1 <semihcakmak1@hotmail.com>
2021-01-26 16:50:33 +0300

    1 file modified

--- index.js
+++ index.js
@@ -5,27 +5,31 @@ import "./style.css";
 const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-//  1----- parametre olarak 2 fonksiyon alan bir fonksiyon geriye  2.fonksiyon 1. sine parametre olarak yazılıyor ve değer döndürülüyor.
-const fonk1 = (f, g) => {
-  return x => {
-    return f(g(x));
-  };
+const FourEach = (arr, fn) => {
+  for (let i = 0; i < arr.length; i++) {
+    fn(arr[i]);
+  }
 };
 
-//2-----çeşitli işlemler yapan fonksiyornlarımız
-const add1 = x => {
-  return x + 1;
-};
-const multi2 = x => {
-  return x * 2;
-};
-const sqr2 = x => {
-  return x * x;
-};
-const res2 = x => {
-  return x % 2;
+let arr1 = [10, 25, 36, 4, 54, 64, 7, 81];
+
+//verilen dizideki elemanın çift mi ?
+const isEven = number => {
+  if (!(number % 2)) {
+    return console.log(number, "is even");
+  }
 };
 
-//3---- 1 nolu fonksiyonu çağırıyoruz ve döndürdüğü değeri denemeye ayıtoruz. denemeye parametre verip  yazdırıyoruz ve sonucu görüyoruz.
-const deneme = fonk1(add1, multi2);
-console.log(deneme(2));
+//verilen dizideki elemanın karekoku var mı ?
+const hasSquareRoot = number => {
+  const sqr = Math.sqrt(number);
+  if ((sqr * 10) % 10 === 0) {
+    let sqrNum = Math.sqrt(number);
+    return console.log(number, "is squre of", sqrNum);
+  }
+};
+
+FourEach(arr1, num => {
+  // isEven(num)
+  hasSquareRoot(num);
+});
