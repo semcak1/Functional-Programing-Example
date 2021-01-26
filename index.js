25e350cabd227b4f6363a427ee938ac4342954cd
semcak1 <semihcakmak1@hotmail.com>
2021-01-26 22:35:06 +0300

    1 file modified

--- index.js
+++ index.js
@@ -5,17 +5,13 @@ import "./style.css";
 const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-// const unary = fn => (fn.length === 1 ? fn : arg => fn(arg));
-
-// const answ = ["1", "2", "3"].map(unary(parseInt));
-// console.log(answ);
-
 const filter = (array, fn) => {
   let result = [];
   for (const value of array) {
     fn(value) ? result.push(value) : undefined;
   }
   return result;
-};ok.rating[0] > 4.5
+};
+ok.rating[0] > 4.5;
 const result = filter([1, 2, 3], val => val <= 0);
 console.log(result);
