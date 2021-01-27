2e15fd0cb761a1a2e3256d052b6d129175455b72
semcak1 <semihcakmak1@hotmail.com>
2021-01-27 21:37:58 +0300

    1 file modified

--- index.js
+++ index.js
@@ -5,13 +5,9 @@ import "./style.css";
 const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-const filter = (array, fn) => {
-  let result = [];
-  for (const value of array) {
-    fn(value) ? result.push(value) : undefined;
-  }
-  return result;
+const curry = fn => {
+  return arg1 => arg2 => fn(arg1, arg2);
 };
+const newFonk = curry((x, y) => x * y);
 
-const result = filter([1, 2, 3], val => val <= 0);
-console.log(result);
+console.log(newFonk(3)(4));
